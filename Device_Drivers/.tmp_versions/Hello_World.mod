6918f029520f1e23bd19a428c9999b80590abc0e
UdayanSinha <udayanprabirsinha@gmail.com>
2016-03-27 21:09:24 +0200

    Cleaned up the directories

--- Device_Drivers/.tmp_versions/Hello_World.mod
+++
@@ -1,2 +0,0 @@
-/home/udayan/Code_Blocks/Device_Drivers/Hello_World.ko
-/home/udayan/Code_Blocks/Device_Drivers/Hello_World.o
--- Device_Drivers/Hello_World/.Hello_World.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/udayan/Code_Blocks/Device_Drivers/Hello_World/Hello_World.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/udayan/Code_Blocks/Device_Drivers/Hello_World/Hello_World.ko /home/udayan/Code_Blocks/Device_Drivers/Hello_World/Hello_World.o /home/udayan/Code_Blocks/Device_Drivers/Hello_World/Hello_World.mod.o
